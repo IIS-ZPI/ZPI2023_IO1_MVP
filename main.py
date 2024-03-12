0286631803b8cd4b5a8575f96a14ca4187144da8
juliusz brodnicki <juliusbrodnicki@gmail.com>
2024-03-12 19:35:34 +0100

    Added power function

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def Division(A: float, B: float) -> float:
     raise Exception("Can not divide by 0")
   return A / B
 
+def IArithmeticsPower(a: int, b: int) -> int:
+   return a**b
+
 print("MVP DevOps sevv00")
 print("kaszczu02 97536423")
 print("JakGin ID: 84508832")
