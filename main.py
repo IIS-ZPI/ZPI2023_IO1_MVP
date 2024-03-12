8450abc6849470bc79bd5a447a21f2c73321053e
Jakub Glinka <jakubglinka9@gmail.com>
2024-03-12 18:43:08 +0100

    Add division function

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ def IArithemeticsAdd(a, b):
 def IArithemeticsDiff(a, b):
     return a-b
 
+def Division(A: float, B: float) -> float:
+  if B == 0:
+    raise Exception("Can not divide by 0")
+  return A / B
+
 print("MVP DevOps sevv00")
 print("kaszczu02 97536423")
 
