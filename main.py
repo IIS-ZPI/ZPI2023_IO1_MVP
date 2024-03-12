a47022011a89251b8846710bdd530781fa092198
Jakub Glinka <jakubglinka9@gmail.com>
2024-03-12 19:13:20 +0100

    Add comment

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+# Add two numbers
 def IArithemeticsAdd(a, b):
     return a+b
 
+# Subtract two numbers
 def IArithemeticsDiff(a, b):
     return a-b
 
+# Multiply two numbers
 def IArithmeticsMult(a, b):
     return a * b
 
+# Divide two numbers
 def Division(A: float, B: float) -> float:
   if B == 0:
     raise Exception("Can not divide by 0")
