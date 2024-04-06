628a39b569677f2fc1255b09c1f2120cd8f70d41
kaszczu02 <240701@edu.p.lodz.pl>
2024-04-06 15:45:08 +0200

    Create base structure

---
+++ src/constans.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class AnalysisPeriod(Enum):
+    LAST_WEEK = 1
+    TWO_WEEKS = 2
+    LAST_MONTH = 3
+    LAST_QUARTER = 4
+    LAST_HALF_YEAR = 5
+    LAST_YEAR = 6
---
+++ tests/test_get_sessions_data.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../')
+
+
+def test_invalid_currency():
+    """
+        Test case for testing function behavior with invalid currency input.
+    """
+    pass
+
+
+def test_invalid_analysis_period():
+    """
+        Test case for testing function behavior with invalid analysis_period.
+    """
+    pass
+
+
