489ba0d6f29417653b71219c192ab5ddb905445d
Jakub Glinka <jakubglinka9@gmail.com>
2024-04-06 20:00:42 +0200

    Rename constant's names

--- src/constans.py
+++ src/constans.py
@@ -2,9 +2,9 @@ from enum import Enum
 
 
 class AnalysisPeriod(Enum):
-    LAST_WEEK = 1
+    WEEK = 1
     TWO_WEEKS = 2
-    LAST_MONTH = 3
-    LAST_QUARTER = 4
-    LAST_HALF_YEAR = 5
-    LAST_YEAR = 6
+    MONTH = 3
+    QUARTER = 4
+    HALF_YEAR = 5
+    YEAR = 6
