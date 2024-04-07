5b4cadaabc724ee9fb2c22281aeb4b99c1ab8e18
Jakub Glinka <jakubglinka9@gmail.com>
2024-04-07 12:01:09 +0200

    Make directories python's packages

--- tests/test_calculate_statistical_measures.py
+++ tests/test_calculate_statistical_measures.py
@@ -1,7 +1,3 @@
-import sys
-sys.path.append('../')
-
-
 def test_invalid_currency():
     """
         Test case for testing function behavior with invalid currency input.
@@ -14,5 +10,3 @@ def test_invalid_analysis_period():
         Test case for testing function behavior with invalid analysis_period.
     """
     pass
-
-
